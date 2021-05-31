6581442a6fa58a58d2c788b30447df11f68cc54d
NicolasZDH <50338048+NicolasZDH@users.noreply.github.com>
2021-05-31 20:35:59 -0300

    fet: loginbox
    Facebook loginbox

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,16 @@
 function Home() {
-    return <div>Home</div>
+    const client_id = process.env.FACEBOOK_CLIENT_ID
+    const redirect_uri = "https://auth.pivotdata.com.br/login"
+
+    const dialogbox = `https://www.facebook.com/v10.0/dialog/oauth?client_id=${client_id}&display=popup&response_type=token&redirect_uri=${redirect_uri}`;
+
+    return (<div>
+        <a target="_blank" href={dialogbox} rel="noopener noreferrer">
+            <button type="button">Login Facebook</button>
+        </a>
+
+    </div>)
 }
 
 export default Home
+
---
+++ pages/login.js
@@ -0,0 +1,5 @@
+function Login() {
+    return (<div>Autenticado!</div>)
+}
+
+export default Login
