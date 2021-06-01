078797c44199f015bdd74f39c2a049efe7336d89
NicolasZDH <50338048+NicolasZDH@users.noreply.github.com>
2021-05-31 21:08:54 -0300

    fet: FbLoginBox
    FacebookLoginBox add status parameter

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 function Home() {
-    const client_id = process.env.FACEBOOK_CLIENT_ID
-    const redirect_uri = "https://auth.pivotdata.com.br/login"
+    const client_id = process.env.FACEBOOK_CLIENT_ID;
+    const redirect_uri = "https://auth.pivotdata.com.br/login";
+    const response_type = 'code';
+    const state = "{st=state123abc,ds=123456789}";
 
-    const dialogbox = `https://www.facebook.com/v10.0/dialog/oauth?client_id=${client_id}&display=popup&response_type=token&redirect_uri=${redirect_uri}`;
+    const dialogbox = `https://www.facebook.com/v10.0/dialog/oauth?client_id=${client_id}&display=popup&response_type=${response_type}&redirect_uri=${redirect_uri}&state=${state}`;
 
     return (<div>
         <a target="_blank" href={dialogbox} rel="noopener noreferrer">
