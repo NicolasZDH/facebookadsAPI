e2ab2bfb04879e517e748cd09aca657288b15fb8
NicolasZDH <50338048+NicolasZDH@users.noreply.github.com>
2021-06-02 18:41:13 -0300

    t3
    t3

--- pages/_app.js
+++ pages/_app.js
@@ -2,12 +2,12 @@ import '../styles/globals.css';
 import { Provider } from "next-auth/client";
 import { AppProps } from "next/app";
 
-function MyApp({ Component, pageProps }: AppProps) {
+const App = ({ Component, pageProps }: AppProps) => {
   return (
     <Provider session={pageProps.session}>
       <Component {...pageProps} />
     </Provider>
   );
-}
+};
 
-export default MyApp
+export default App;
